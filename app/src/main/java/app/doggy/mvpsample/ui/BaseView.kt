14a60b3d8b4ea24a8fc219ab45b97a0800ab7e28
Kaito-Dogi <kendobu0405@gmail.com>
2023-04-30 05:00:01 +0900

    :bulb: コメントの追記

--- app/src/main/java/app/doggy/mvpsample/ui/BaseView.kt
+++ app/src/main/java/app/doggy/mvpsample/ui/BaseView.kt
@@ -1,7 +1,7 @@
 package app.doggy.mvpsample.ui
 
 /**
- * View で Presenter を DI し忘れることを防ぐ
+ * View で Presenter を DI し忘れることを防ぐため presenter を定義した
  */
 internal interface BaseView<T> {
   val presenter: T
--- app/src/main/java/app/doggy/mvpsample/ui/count/CountContract.kt
+++ app/src/main/java/app/doggy/mvpsample/ui/count/CountContract.kt
@@ -4,6 +4,9 @@ import app.doggy.mvpsample.domain.model.Count
 import app.doggy.mvpsample.ui.BasePresenter
 import app.doggy.mvpsample.ui.BaseView
 
+/**
+ * View, Presenter 以外に継承されることを防ぐため sealed interface にした
+ */
 internal sealed interface CountContract {
   interface View : BaseView<Presenter> {
     fun showCount(count: Count)
