212bc51fb9b9cec1d00e3faee000d0aeffee1f9b
Kaito-Dogi <kendobu0405@gmail.com>
2023-04-30 03:01:47 +0900

    :heavy_plus_sign: ViewBinding の導入

--- app/src/main/java/app/doggy/mvpsample/ui/count/CountFragment.kt
+++ app/src/main/java/app/doggy/mvpsample/ui/count/CountFragment.kt
@@ -1,8 +1,30 @@
 package app.doggy.mvpsample.ui.count
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import app.doggy.mvpsample.R
+import app.doggy.mvpsample.databinding.FragmentCountBinding
+import app.doggy.mvpsample.domain.model.Count
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class CountFragment : Fragment(R.layout.fragment_count)
+class CountFragment : Fragment(R.layout.fragment_count), CountContract.View {
+
+  private var _binding: FragmentCountBinding? = null
+  private val binding: FragmentCountBinding get() = _binding!!
+
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+    _binding = FragmentCountBinding.bind(view)
+  }
+
+  override fun onDestroyView() {
+    super.onDestroyView()
+    _binding = null
+  }
+
+  override fun showCount(count: Count) {
+    binding
+  }
+}
