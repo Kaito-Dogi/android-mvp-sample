182d0b8dbaba7480abf6d9e871a9ca9c1f3ec9fb
Kaito-Dogi <kendobu0405@gmail.com>
2023-04-30 02:31:47 +0900

    :sparkles: CountFragment を追加

---
+++ app/src/main/java/app/doggy/mvpsample/ui/count/CountFragment.kt
@@ -0,0 +1,8 @@
+package app.doggy.mvpsample.ui.count
+
+import androidx.fragment.app.Fragment
+import app.doggy.mvpsample.R
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class CountFragment : Fragment(R.layout.fragment_count)
