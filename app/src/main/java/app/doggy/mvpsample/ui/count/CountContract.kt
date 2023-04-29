b8106723cf4eedef5fc9a9ca841d20681bc82e70
Kaito-Dogi <kendobu0405@gmail.com>
2023-04-30 04:39:02 +0900

    :recycle: BaseView の定義

--- app/src/main/java/app/doggy/mvpsample/ui/count/CountContract.kt
+++ app/src/main/java/app/doggy/mvpsample/ui/count/CountContract.kt
@@ -1,9 +1,10 @@
 package app.doggy.mvpsample.ui.count
 
 import app.doggy.mvpsample.domain.model.Count
+import app.doggy.mvpsample.ui.BaseView
 
 sealed interface CountContract {
-  interface View {
+  interface View : BaseView<Presenter> {
     fun showCount(count: Count)
   }
 
